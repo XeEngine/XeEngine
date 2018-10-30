d7c168b788dccf858269ec2f19c9eb2f97daa8b3
Luciano Ciccariello <xeeynamo@hotmail.com>
2018-10-30 21:36:52 +0000

    Refactoring on Xe::ObjPtr

--- include/XeSDK/XeObjPtr.h
+++ include/XeSDK/XeObjPtr.h
@@ -2,60 +2,108 @@
 #include <XeSDK/IObject.h>
 
 namespace Xe {
-	//! \brief Manage automatically references of an IObject
+	//! \brief Smart pointer that manages the references of an IObject
 	template <typename T>
 	class ObjPtr {
 		T* m_obj;
+
 	public:
+		//! \brief Does not hold any reference
 		ObjPtr() : m_obj(nullptr) {}
-		ObjPtr(T* obj) : m_obj(obj) {
-		}
-		ObjPtr(const ObjPtr& objptr) : m_obj(objptr.m_obj) {
+		
+		//! \brief Assign an IObject without to add a reference
+		ObjPtr(T* obj) : m_obj(obj) {}
+
+		//! \brief Assign an ObjPtr adding a reference
+		ObjPtr(const ObjPtr& objptr) :
+			m_obj(objptr.m_obj)
+		{
 			if (m_obj) m_obj->AddRef();
 		}
-		~ObjPtr() {
+
+		//! \brief Remove a reference from the holding IObject, if exists
+		~ObjPtr()
+		{
 			if (m_obj) m_obj->Release();
 		}
 
-		//! \brief Get the associated object
+		//! \brief Get the raw pointer; does not add any reference
 		T* Get() const { return m_obj; }
 
+		//! \brief Cast to the raw pointer; does not add any reference
 		operator T*() { return m_obj; }
+
+		//! \brief Get a pointer to the raw pointer
 		T** operator&() { return &m_obj; }
+
+		//! \brief Resolve the raw pointer
 		T& operator*() const { return *m_obj; }
+
+		//! \brief Use the raw pointer
 		T* operator->() const { return m_obj; }
+
+		//! \brief Use the helper operator () for IDelegate
+		template <typename T>
+		void operator()(T arg) { m_obj(arg); }
+
+		//! \brief Check if there is an IObject alive
+		operator bool() const { return !!m_obj; }
+
+		//! \brief Check if there is not an IObject alive
 		bool operator!() const { return !m_obj; }
-		ObjPtr<T>& operator=(const ObjPtr<T>& objptr) {
-			T* obj = objptr.m_obj;
-			if (obj) obj->AddRef();
-			if (m_obj) m_obj->Release();
-			m_obj = obj;
-			return *this;
-		}
-		ObjPtr<T>& operator=(T* obj) {
-			if (obj) obj->AddRef();
-			if (m_obj) m_obj->Release();
-			m_obj = obj;
-			return *this;
-		}
-		bool operator==(const T* o) {
+
+		// Commented because in theory the ObjPtr state should not change after
+		// its initialization. This will remain uncommented because I am not
+		// sure if deleting it completely is the right decision.
+		//ObjPtr<T>& operator=(const ObjPtr<T>& objptr)
+		//{
+		//	T* obj = objptr.m_obj;
+		//	if (obj) obj->AddRef();
+		//	if (m_obj) m_obj->Release();
+		//	m_obj = obj;
+		//	return *this;
+		//}
+
+		// Commented because in theory the ObjPtr state should not change after
+		// its initialization. This will remain uncommented because I am not
+		// sure if deleting it completely is the right decision.
+		//ObjPtr<T>& operator=(T* obj)
+		//{
+		//	if (obj) obj->AddRef();
+		//	if (m_obj) m_obj->Release();
+		//	m_obj = obj;
+		//	return *this;
+		//}
+
+		bool operator==(const T* o)
+		{
 			return m_obj == o;
 		}
-		bool operator==(const ObjPtr<T>& o) {
+
+		bool operator==(const ObjPtr<T>& o)
+		{
 			return m_obj == o.m_obj;
 		}
+
 		template <typename U>
-		bool operator==(const ObjPtr<U>& o) {
+		bool operator==(const ObjPtr<U>& o)
+		{
 			return m_obj == o.m_obj;
 		}
-		bool operator!=(const T* o) {
+
+		bool operator!=(const T* o)
+		{
 			return m_obj != o;
 		}
-		bool operator!=(const ObjPtr<T>& o) {
+
+		bool operator!=(const ObjPtr<T>& o)
+		{
 			return m_obj != o.m_obj;
 		}
+
 		template <typename U>
-		bool operator!=(const ObjPtr<U>& o) {
+		bool operator!=(const ObjPtr<U>& o)
+		{
 			return m_obj != o.m_obj;
 		}
 	};
