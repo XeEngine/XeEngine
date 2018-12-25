e39d9b34124a0b35ce28a9894ba82f9dc7413f79
Luciano Ciccariello <xeeynamo@hotmail.com>
2018-12-25 12:50:32 +0100

    Add Imaging::OpenImage utility to use the most appropiate image driver

--- include/XeSDK/XeGraphicsImaging.h
+++ include/XeSDK/XeGraphicsImaging.h
@@ -1,12 +1,15 @@
 #pragma once
 #include <XeSDK/IGraphicsContext.h>
 #include <XeSDK/IGraphicsImagingImage.h>
+#include <XeSDK/XeIO.h>
+#include "IStorage.h"
 
-namespace Xe {
-	namespace Graphics {
-		//! \brief Image format management
-		namespace Imaging {
-			bool CreateSurface(IContext *context, ISurface **surface, SurfaceType type, IImage *image);
-		}
-	}
-}
+namespace Xe { namespace Graphics { namespace Imaging {
+	//! \brief Try to open the most appropiate image from the file extension
+	bool OpenImage(IImage** ppImage, Xe::IO::IFileStream* pStream);
+
+	//! \brief Try to open the most appropiate image from the file extension
+	bool OpenImage(IImage** ppImage, Xe::Storage::IStorage* pStorage, const char* fileName);
+	
+	bool CreateSurface(IContext *context, ISurface **surface, SurfaceType type, IImage *image);
+} } }
--- src/XeSDK/XeGraphicsImaging.cpp
+++ src/XeSDK/XeGraphicsImaging.cpp
@@ -1,86 +1,108 @@
 #include "pch.h"
 #include <XeSDK/XeGraphicsImaging.h>
 #include <XeSDK/XeMemory.h>
+#include <XeSDK/XeStoragePath.h>
+#include "XeSDK/XeObjPtr.h"
+#include "XeSDK/XeGraphicsImagingPng.h"
 
-namespace Xe {
-    namespace Graphics {
-        namespace Imaging {
-            bool CreateSurface(IContext *context, ISurface **surface, SurfaceType type, IImage *image) {
-                context->AddRef();
-                image->AddRef();
+using namespace Xe;
+using namespace Xe::Math;
+using namespace Xe::IO;
+using namespace Xe::Graphics;
+using namespace Xe::Graphics::Imaging;
 
-                const Size &size = image->GetSize();
-                Color::Format format = image->GetFormat();
-                svar bpp = Color::GetBitsPerPixel(format);
-                if (bpp == 24) bpp = 32; // APPLY CONVERSION
-                else if (bpp == 4) bpp = 8;
-                DataDesc desc;
-                desc.pitch = size.x * bpp / 8;
-                u8 *data = (u8*)Memory::Alloc(desc.pitch * size.y);
-                desc.data = data;
+bool Imaging::OpenImage(IImage** ppImage, Xe::IO::IFileStream* pStream)
+{
+	const auto ext = Xe::Storage::Path::GetExtension(pStream->GetFileName());
 
-                switch (format) {
-                    case Color::Format_RGB888: {
-                        uvar bufferPitch = size.x * 3;
-                        u8 *buffer = (u8*)Memory::Alloc(bufferPitch);
-                        u8 *pData = data;
-                        for (s32 i = 0; i < size.y; i++) {
-                            u8 *pBuffer = buffer;
-                            image->ReadLine(buffer, 0);
-                            for (s32 j = 0; j < size.x; j++) {
-                                *pData++ = *pBuffer++;
-                                *pData++ = *pBuffer++;
-                                *pData++ = *pBuffer++;
-                                *pData++ = 0xFF;
-                            }
-                        }
-                        Memory::Free(buffer);
-                        format = Color::Format_RGBX8888;
-                    } break;
-                    case Color::Format_BGR888: {
-                        uvar bufferPitch = size.x * 3;
-                        u8 *buffer = (u8*)Memory::Alloc(bufferPitch);
-                        u8 *pData = data;
-                        for (s32 i = 0; i < size.y; i++) {
-                            u8 *pBuffer = buffer;
-                            image->ReadLine(buffer, 0);
-                            for (s32 j = 0; j < size.x; j++) {
-                                *pData++ = *pBuffer++;
-                                *pData++ = *pBuffer++;
-                                *pData++ = *pBuffer++;
-                                *pData++ = 0xFF;
-                            }
-                        }
-                        Memory::Free(buffer);
-                        format = Color::Format_BGRX8888;
-                    } break;
-                    case Color::Format_Indexed4: {
-                        uvar bufferPitch = (size.x + 1) >> 1;
-                        u8 *buffer = (u8*)Memory::Alloc(bufferPitch);
-                        for (s32 i = 0; i < size.y; i++) {
-                            image->ReadLine(buffer, 0);
-                            u8 *pSrc = buffer;
-                            u8 *pDst = data + desc.pitch * i;
-                            for (uvar i = 0; i < bufferPitch; i++) {
-                                *pDst++ = (*pSrc >> 4) & 0x0F;
-                                *pDst++ = *pSrc & 0x0F;
-                                pSrc++;
-                            }
-                        }
-                        Memory::Free(buffer);
-                        format = Color::Format_Indexed8;
-                    } break;
-                    default:
-                        for (s32 i = 0; i < size.y; i++)
-                            image->ReadLine(data, desc.pitch * i);
-                }
-                bool res = context->CreateSurface(surface, type, size, format, desc);
+	if (String::CompareInsensitive(ext, ".png"))
+		return PngOpen(ppImage, pStream);
 
-                Memory::Free(data);
-                image->Release();
-                context->Release();
-                return res;
-            }
-        }
-    }
+	return false;
+}
+
+bool Imaging::OpenImage(IImage** ppImage, Xe::Storage::IStorage* pStorage, const char* fileName)
+{
+	ObjPtr<IFileStream> fileStream;
+	if (pStorage->FileOpen(&fileStream, fileName, Storage::Access_Read, Storage::Mode_Open) != Error::OK)
+		return false;
+
+	return OpenImage(ppImage, fileStream);
+}
+
+bool Imaging::CreateSurface(Xe::Graphics::IContext *context, ISurface **surface, SurfaceType type, IImage *image) {
+	context->AddRef();
+	image->AddRef();
+
+	const Size &size = image->GetSize();
+	Color::Format format = image->GetFormat();
+	svar bpp = Color::GetBitsPerPixel(format);
+	if (bpp == 24) bpp = 32; // APPLY CONVERSION
+	else if (bpp == 4) bpp = 8;
+	DataDesc desc;
+	desc.pitch = size.x * bpp / 8;
+	u8 *data = (u8*)Memory::Alloc(desc.pitch * size.y);
+	desc.data = data;
+
+	switch (format) {
+	case Color::Format_RGB888: {
+		uvar bufferPitch = size.x * 3;
+		u8 *buffer = (u8*)Memory::Alloc(bufferPitch);
+		u8 *pData = data;
+		for (s32 i = 0; i < size.y; i++) {
+			u8 *pBuffer = buffer;
+			image->ReadLine(buffer, 0);
+			for (s32 j = 0; j < size.x; j++) {
+				*pData++ = *pBuffer++;
+				*pData++ = *pBuffer++;
+				*pData++ = *pBuffer++;
+				*pData++ = 0xFF;
+			}
+		}
+		Memory::Free(buffer);
+		format = Color::Format_RGBX8888;
+	} break;
+	case Color::Format_BGR888: {
+		uvar bufferPitch = size.x * 3;
+		u8 *buffer = (u8*)Memory::Alloc(bufferPitch);
+		u8 *pData = data;
+		for (s32 i = 0; i < size.y; i++) {
+			u8 *pBuffer = buffer;
+			image->ReadLine(buffer, 0);
+			for (s32 j = 0; j < size.x; j++) {
+				*pData++ = *pBuffer++;
+				*pData++ = *pBuffer++;
+				*pData++ = *pBuffer++;
+				*pData++ = 0xFF;
+			}
+		}
+		Memory::Free(buffer);
+		format = Color::Format_BGRX8888;
+	} break;
+	case Color::Format_Indexed4: {
+		uvar bufferPitch = (size.x + 1) >> 1;
+		u8 *buffer = (u8*)Memory::Alloc(bufferPitch);
+		for (s32 i = 0; i < size.y; i++) {
+			image->ReadLine(buffer, 0);
+			u8 *pSrc = buffer;
+			u8 *pDst = data + desc.pitch * i;
+			for (uvar i = 0; i < bufferPitch; i++) {
+				*pDst++ = (*pSrc >> 4) & 0x0F;
+				*pDst++ = *pSrc & 0x0F;
+				pSrc++;
+			}
+		}
+		Memory::Free(buffer);
+		format = Color::Format_Indexed8;
+	} break;
+	default:
+		for (s32 i = 0; i < size.y; i++)
+			image->ReadLine(data, desc.pitch * i);
+	}
+	bool res = context->CreateSurface(surface, type, size, format, desc);
+
+	Memory::Free(data);
+	image->Release();
+	context->Release();
+	return res;
 }
